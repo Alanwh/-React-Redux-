5dc567f10bf58dc30ec043e3681e585853008faf
hewj <wenjunhe@ctrip.com>
2018-10-09 12:03:09 +0800

    计数器

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.handleCount = this.handleCount.bind(this);
+        this.state = {
+            count: 0
+        }
+    }
+
+    handleCount() {
+        this.setState({
+            count: this.state.count+1
+        })
+    }
+
     render() {
+        const style = {
+            'margin': '16px'
+        }
         return(
-            <div>
-                hello world!
+            <div style={style}>
+                <button onClick={this.handleCount}>点击</button>  
+                <p>计数器：{ this.state.count }</p>    
             </div>
         )
     }
